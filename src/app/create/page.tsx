8d6bc42c6c548cbcc967651bb3a7c3a8de45c218
AnirudhVijayaraghavan <114264437+AnirudhVijayaraghavan@users.noreply.github.com>
2023-12-12 05:21:38 -0500

    cdtest2
    cdtest2

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -16,7 +16,7 @@ const CreatePage = async (props: Props) => {
   return (
     <div className="flex flex-col items-start max-w-xl px-8 mx-auto my-16 sm:px-0">
       <h1 className="self-center text-3xl font-bold text-center sm:text-6xl">
-        Your Learning Journey
+        Your Curated AI Assistant.
       </h1>
       <div className="flex p-4 mt-5 border-none bg-secondary">
         <InfoIcon className="w-12 h-12 mr-3 text-blue-400" />
