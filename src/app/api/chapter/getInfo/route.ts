1f6510f6b8ae48c523fd833abf13b6668e21b627
SunilS97 <111933521+SunilS97@users.noreply.github.com>
2023-12-15 00:54:36 -0500

    Update route.ts prompting

--- src/app/api/chapter/getInfo/route.ts
+++ src/app/api/chapter/getInfo/route.ts
@@ -77,8 +77,8 @@ export async function POST(req: Request, res: Response) {
       },
     });
 
-    // return NextResponse.json({ success: true });
-    return NextResponse.json({ transcript, summary});
+    return NextResponse.json({ success: true });
+    
   } catch (error) {
     if (error instanceof z.ZodError) {
       return NextResponse.json(
